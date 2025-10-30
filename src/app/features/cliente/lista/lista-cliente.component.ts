e6de3f046309672eeca6033b0a861d3a327d11c6
camilo.cabrera <vladimircabrecabri@gmail.com>
2025-10-29 23:16:48 -0400

    feat: actualizar lista de clientes y refactorizar metodo de obtencion de data

--- src/app/features/cliente/lista/lista-cliente.component.ts
+++ src/app/features/cliente/lista/lista-cliente.component.ts
@@ -23,7 +23,6 @@ import {TooltipModule} from "primeng/tooltip";
     TagModule,
     SliderModule,
     ProgressBarModule,
-    NgClass,
     DatePipe,
     CurrencyPipe,
     PaginatorModule,
@@ -52,34 +51,8 @@ export class ListaClienteComponent implements OnInit {
   }
 
   ngOnInit() {
-    this.clienteService.getCustomersLarge().then((customers) => {
-      this.customers = customers;
-      this.loading = false;
+    this.getCustomersLarge();
 
-      this.customers.forEach((customer) => (customer.date = new Date(<Date>customer.date)));
-    });
-
-    this.representatives = [
-      { name: 'Amy Elsner', image: 'amyelsner.png' },
-      { name: 'Anna Fali', image: 'annafali.png' },
-      { name: 'Asiya Javayant', image: 'asiyajavayant.png' },
-      { name: 'Bernardo Dominic', image: 'bernardodominic.png' },
-      { name: 'Elwin Sharvill', image: 'elwinsharvill.png' },
-      { name: 'Ioni Bowcher', image: 'ionibowcher.png' },
-      { name: 'Ivan Magalhaes', image: 'ivanmagalhaes.png' },
-      { name: 'Onyama Limba', image: 'onyamalimba.png' },
-      { name: 'Stephen Shaw', image: 'stephenshaw.png' },
-      { name: 'Xuxue Feng', image: 'xuxuefeng.png' }
-    ];
-
-    this.statuses = [
-      { label: 'Unqualified', value: 'unqualified' },
-      { label: 'Qualified', value: 'qualified' },
-      { label: 'New', value: 'new' },
-      { label: 'Negotiation', value: 'negotiation' },
-      { label: 'Renewal', value: 'renewal' },
-      { label: 'Proposal', value: 'proposal' }
-    ];
   }
 
   clear(table: Table) {
@@ -113,5 +86,35 @@ export class ListaClienteComponent implements OnInit {
 
   deleteCustomer(customer: any) {
 
+  }
+
+  public getCustomersLarge() {
+    this.clienteService.getCustomersLarge().then((customers) => {
+      this.customers = customers;
+      this.loading = false;
+      this.customers.forEach((customer) => (customer.date = new Date(<Date>customer.date)));
+    });
+
+    this.representatives = [
+      {name: 'Amy Elsner', image: 'amyelsner.png'},
+      {name: 'Anna Fali', image: 'annafali.png'},
+      {name: 'Asiya Javayant', image: 'asiyajavayant.png'},
+      {name: 'Bernardo Dominic', image: 'bernardodominic.png'},
+      {name: 'Elwin Sharvill', image: 'elwinsharvill.png'},
+      {name: 'Ioni Bowcher', image: 'ionibowcher.png'},
+      {name: 'Ivan Magalhaes', image: 'ivanmagalhaes.png'},
+      {name: 'Onyama Limba', image: 'onyamalimba.png'},
+      {name: 'Stephen Shaw', image: 'stephenshaw.png'},
+      {name: 'Xuxue Feng', image: 'xuxuefeng.png'}
+    ];
+
+    this.statuses = [
+      {label: 'Unqualified', value: 'unqualified'},
+      {label: 'Qualified', value: 'qualified'},
+      {label: 'New', value: 'new'},
+      {label: 'Negotiation', value: 'negotiation'},
+      {label: 'Renewal', value: 'renewal'},
+      {label: 'Proposal', value: 'proposal'}
+    ];
   }
 }
