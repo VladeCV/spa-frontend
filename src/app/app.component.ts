7e7ebb18f3ba28619f6b4fbb2b23171adfe20651
camilo.cabrera <camilo.cabrera@vicepresidencia.gob.bo>
2025-10-31 11:23:18 -0400

    fix: adicionar tags para los mensajes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,24 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import {ButtonDirective} from "primeng/button";
-import {ListaClienteComponent} from "./features/cliente/lista/lista-cliente.component";
+import { ButtonDirective } from "primeng/button";
+import { ListaClienteComponent } from "./features/cliente/lista/lista-cliente.component";
+import { ToastModule } from "primeng/toast";
+import { ConfirmationService, MessageService } from "primeng/api";
+import { ConfirmPopupModule } from "primeng/confirmpopup";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ButtonDirective, ListaClienteComponent],
+  imports: [
+    RouterOutlet,
+    ButtonDirective,
+    ListaClienteComponent,
+    ToastModule,
+    ConfirmPopupModule
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrls: ['./app.component.scss'],
+  providers: [MessageService, ConfirmationService]
 })
 export class AppComponent {
   title = 'sap-frontend';
